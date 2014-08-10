0fe3768689f1c0ce32df7c86928ca607dcbe25a4
alexgzhou <alexgzhou@163.com>
2014-08-10 23:04:52 +0800

    ionicAnimation removed from ionic cause problem

---
+++ www/js/icyl/controllers.js
@@ -0,0 +1,226 @@
+angular.module('icyl.controllers', [])
+
+.controller('index', ['$scope', function ($scope) {
+
+  // ionic.Platform.ready(function() {
+  //   // hide the status bar using the StatusBar plugin
+  //   if(window.StatusBar) {
+  //     // org.apache.cordova.statusbar required
+  //     StatusBar.hide();
+  //   }
+    
+  // });
+
+}])
+
+//主容器控制器
+.controller('mainContainer', ['$scope', '$state', 'Identification', 'User', 'Alert', 'CustomNav', '$window', function ($scope, $state, Identification, User, Alert, CustomNav, $window) {
+
+	// var statics = 1;
+	// console.log('not execute everytime, just one time #'+statics);
+	// statics += 1;
+
+  //CustomNav.new();
+
+  // $scope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams){
+  //   if (toState.access.authenticate && !Identification.isAuthenticated()) {
+  //     //console.log(Identification.isAuthenticated());  //=====================test
+  //     if (!!toState.access.offline && toState.access.offline != fromState.name) {
+  //       $state.go(toState.access.offline);
+  //     }
+  //     else {
+  //       Identification.checkToken().then( function (data) {
+  //         if (data.err_code != 0) {
+  //           $scope.actions = {
+  //             toState: toState
+  //           };
+  //           User.userLogin($scope);
+  //           User.userRegister($scope);
+  //           //console.log($scope.actions.login);  //=====================test
+            
+  //         }
+  //         else {
+  //           $state.go(toState);
+  //         }
+  //       }, function (err) {
+  //         console.log('错误：Identification.checkToken()' + err);
+  //         Alert('请检查网络！');
+  //       });
+  //     }
+      
+  //     //$state.transitionTo("login");
+  //     event.preventDefault(); 
+  //   }
+  // });
+
+  // $scope.$on("$stateChangeSuccess", function (event, toState, toParams, fromState, fromParams){
+  //   var index = -1;
+  // 	if (!!fromState.name && fromState.name.indexOf('offline') < 0) {
+  // 		index = CustomNav.find(toState.name);
+  // 		if (index < 0 && toState.name != fromState.name) {
+  // 			CustomNav.record(fromState.name);
+  // 			//console.log('record');
+  // 		}
+  // 		else {
+  // 			CustomNav.remove(0, index+1);
+  // 			//console.log('remove');
+  // 		}
+  		
+  // 		//console.log('in');  //=====================test
+  // 	}
+  //   //console.log(CustomNav.histories+'==='+fromState.name+'==='+index+'==='+toState.name+'==='+$state.current.name);  //=====================test
+  //   fromState.name.indexOf('offline') > -1 ? CustomNav.fromState = fromState.name : CustomNav.fromState = '';
+
+  // });
+
+
+
+}])
+
+//默认主页控制器
+.controller('mainDefault', ['$scope', '$window', 'Storage', function($scope, $window, Storage) {
+
+    $scope.url = {
+      sysmgmt: { url: 'http://17f.go5le.net/mall/index/login_app.asp'},
+      news   : { url: 'http://17f.go5le.net/99_tj/991/index.asp?lx=%CD%C5%C7%E0%B6%AF%CC%AC'},
+      test   : { url: 'http://www.baidu.com'}
+    };
+
+    $scope.cookie = 'xsunion=staff%5Fsts=2&telephone=0571%2D83731771&card5=900000001&name=900006840&dw=%B3%F8%C1%F4%CF%E3%B4%A8%B2%CB%BB%F0%B9%F8&card4=900000002&card2=900006840&card%5Fno1=900006840&shopid1=900000003&staff%5Fgrade=1&reg%5Fnbr=900006840&card3=900000003';//Storage.kget('cookie');
+
+    // var inAppBrowser = $window.open('http://17f.go5le.net/preload.html','_blank','hidden=yes');
+    // var setcookie = function() {
+    //   inAppBrowser.executeScript({
+    //     code: "document.cookie='xsunion=staff%5Fsts=2&telephone=0571%2D83731771&card5=900000001&name=900006840&dw=%B3%F8%C1%F4%CF%E3%B4%A8%B2%CB%BB%F0%B9%F8&card4=900000002&card2=900006840&card%5Fno1=900006840&shopid1=900000003&staff%5Fgrade=1&reg%5Fnbr=900006840&card3=900000003';"
+    //   });
+    // };
+    // // var closeonce = function() {
+    // //   inAppBrowser.removeEventListener('loadstart', setcookie);
+    // //   inAppBrowser.removeEventListener('loadstop', closeonce);
+    // //   //inAppBrowser.close();
+    // // };
+    // // inAppBrowser.addEventListener('loadstart', setcookie);
+    // inAppBrowser.addEventListener('loadstop', setcookie);
+    // inAppBrowser.removeEventListener('loadstop', setcookie);
+    // inAppBrowser.close();
+
+              
+          
+    
+
+
+    $scope.actions = [];
+    $scope.closeBrowser = function(){
+      //$window.alert($scope.actions.toString());    //====================test
+      $scope.actions.push("Exit");
+    };
+    $scope.loadOpen = function(){
+      //$window.alert('Load Open');
+      $scope.actions.push("Open");
+    };
+    $scope.loadStop = function(){
+      //$window.alert('Load Stop #1');    //====================test
+      //$window.alert('Load Stop #2');    //====================test
+      $scope.actions.push("Stop");
+    };
+    $scope.loadError = function(){
+      //$window.alert('Load Error');    //====================test
+      $scope.actions.push("Error");
+    };
+
+}])
+
+//用户在线页面控制器
+.controller('mainMine', ['$scope', '$ionicNavBarDelegate', '$timeout', function($scope, $ionicNavBarDelegate, $timeout) {
+
+  $scope.hidebackbutton = function () {
+    //$ionicNavBarDelegate.showBackButton(false); //这个方法不能在页面加载时被执行
+    $ionicNavBarDelegate.setTitle('hahahahaha'); //这个方法不能在页面加载时被执行
+  };
+
+  // if (!!CustomNav.fromState) {
+  //   $timeout(function(){  //这个方法不能在页面加载时被执行, use $timeout as a trick
+  //     $ionicNavBarDelegate.showBackButton(false);
+  //   },1);
+  // }
+    
+	
+
+}])
+//用户离线页面控制器
+.controller('mainMineOffline', ['$scope', function($scope) {
+
+
+
+}])
+
+//用户账户控制器
+.controller('mainAccount', ['$scope', 'User', function($scope, User) {
+
+	$scope.actions = {
+		logout: function () {
+			User.userLogout();
+		}
+	};
+
+}])
+
+//用户信息控制器
+.controller('mainUserInfo', ['$scope', '$state', 'User', 'Identification', 'Alert', function($scope, $state, User, Identification, Alert) {
+
+	User.getUserInfo().then( function (data) {
+	  if (data.err_code == 0) {
+		//console.log(data.data); //=====================test
+	    $scope.updateUserInfo = {
+	      name: data.data.Name,
+	      gender: !!Number(data.data.Gender),
+	      birthday: data.data.Birthday,
+	      mobile: data.data.Mobile,
+	      email: data.data.Email
+	    };
+	    //console.log($scope.updateUserInfo); //=====================test
+	  }
+	  else {
+	    //console.log('#1--------------'); //=====================test
+	    $scope.actions = {};
+	    User.userLogin($scope);
+	    User.userRegister($scope);
+	  }
+	}, function (err) {
+	  console.log('错误：User.getUserInfo()' + err);
+      Alert('请检查网络！');
+	});
+
+  //console.log('again'); //=====================test
+
+  $scope.actions = {
+    update: function () {
+      Identification.checkToken().then( function (data) {
+        User.updateUserInfo($scope.updateUserInfo).then( function (data) {
+          if (data.err_code == 0) {
+            //console.log(data.data); //=====================test
+            $state.go('main.account');
+          }
+          else {
+            //console.log(data); //=====================test
+            $scope.actions = {};
+            User.userLogin($scope);
+            User.userRegister($scope);
+          }
+        }, function (err) {
+          console.log('错误：User.updateUserInfo()' + err);
+          Alert('请检查网络！');
+        });
+      }, function (err) {
+        console.log('错误：Identification.checkToken()' + err);
+        Alert('请检查网络！');
+      })
+      
+    }
+  };
+
+
+
+
+
+}]);
---
+++ www/js/icyl/directives.js
@@ -0,0 +1,203 @@
+angular.module('icyl.directives', [])
+
+//自定义返回按钮
+.directive( "customGoBack", ['$state', 'CustomNav', function($state, CustomNav) {
+    return {
+        restrict: "A",
+        link: function( scope, element, attrs ) {
+            element.bind( "click", function () {
+                if (!!CustomNav.fromState) {
+                    $state.go(CustomNav.fromState);
+                }
+                else if (!!CustomNav.goback()) {
+                    $state.go(CustomNav.goback());
+                }
+                else {
+                    $state.go(CustomNav.defaultback($state.current.name));
+                }
+			    //!!CustomNav.goback() ? $state.go(CustomNav.goback()) : $state.go(CustomNav.defaultback($state.current.name));
+			});
+        }
+    }
+}])
+
+// .directive( "customGoBackOffline", ['$state', 'CustomNav', function($state, CustomNav) {
+//     return {
+//         restrict: "A",
+//             link: function( scope, element, attrs ) {
+//             element.bind( "click", function () {
+// 			    !!CustomNav.fromState ? $state.go(CustomNav.fromState) : $state.go(CustomNav.defaultback($state.current.name));
+// 			});
+//         }
+//     }
+// }])
+
+// //在线校验指令按钮
+// .directive( "offlineCheck", ['$window', 'CustomNav', function($window, CustomNav) {
+//     return {
+//         restrict: "A",
+//         link: function( scope, element, attrs ) {
+//             element.bind( "click", function () {
+            
+//             });
+//         }
+//     }
+// }])
+
+//打开外部页面按钮：封装了Cordova插件inAppBrowser
+.directive("openExternal", function($window){
+    return{
+        restrict: 'E',
+        scope: {
+            url : "=",
+            cookie: "=",
+            exit : "&",
+            loadOpen : "&",
+            loadStop : "&",
+            loadError: "&"
+        },
+        replace:true,
+        transclude: true,
+        template:"<a class='button center-block background-clear' ng-click='openUrl()'><span ng-transclude></span></a>",
+        // link: function( scope, element, attrs ) {
+        //     console.log(scope.$id);    //====================test
+        // },
+        controller: function($scope){
+
+            // var wrappedFunction = function(action){
+            //     return function(){
+            //         $scope.$apply(function(){
+            //             action();
+            //         });
+            //     }
+            // };
+
+            var inAppBrowserClosed = function() {
+                if(inAppBrowser != null){
+                    //console.log("did it");    //====================test
+                    //inAppBrowser.removeEventListener('exit', wrappedFunction($scope.exit));
+                    if($scope.loadOpen){
+                        inAppBrowser.removeEventListener('loadstart', inAppBrowserStart);
+                        //$window.alert('removeEventListener: $scope.loadStart');    //====================test
+                    }
+                    if($scope.loadStop){
+                        inAppBrowser.removeEventListener('loadstop', inAppBrowserStop);
+                        //$window.alert('removeEventListener: $scope.loadStop');    //====================test
+                    }
+                    if($scope.loadError){
+                        inAppBrowser.removeEventListener('loaderror', $scope.loadError);
+                        //$window.alert('removeEventListener: $scope.loadError');    //====================test
+                    }
+                    if($scope.exit){
+                        inAppBrowser.removeEventListener('exit', inAppBrowserClosed);
+                        //$window.alert('removeEventListener: $scope.exit');    //====================test
+                    }
+                    $scope.exit();
+                }
+            }
+            var inAppBrowserStart = function() {
+                if(inAppBrowser != null){
+                    if($scope.loadOpen){
+                        //$window.alert($scope.url);
+                        inAppBrowser.executeScript({
+                                code: 'document.cookie="' + $scope.cookie + '";'
+                            },
+                            function(values) {
+                                //$window.alert('document.cookie======'+values[0]);
+                            }
+                        );
+                        //inAppBrowser.removeEventListener('loadstart', inAppBrowserStart);
+                        $scope.loadOpen();
+                    }
+                }
+            }
+            var inAppBrowserStop = function() {
+                if(inAppBrowser != null){
+                    if($scope.loadStop){
+                        //$window.alert($scope.url);
+                        inAppBrowser.executeScript({
+                                code: 'document.cookie.match(new RegExp("(^| )xsunion=([^;]*)(;|$)"));'
+                            },
+                            function(values) {
+                                //$window.alert('document.cookie.match======'+values[0][0]);
+                            }
+                        );
+                        //inAppBrowser.removeEventListener('loadstop', inAppBrowserStop);
+                        $scope.loadStop();
+                    }
+                }
+            }
+            //console.log("did it");    //====================test
+            //$scope.exit(); //====================test
+            //console.log($scope.$id);    //====================test
+            var inAppBrowser;// = $window.open('http://17f.go5le.net/preload.html','_blank','hidden=yes');
+            $scope.openUrl = function(){
+                inAppBrowser = $window.open(encodeURI($scope.url),'_blank','location=yes');
+                // inAppBrowser.executeScript({
+                //     code: "document.cookie = 'xsunion=staff%5Fsts=2&telephone=0571%2D83731771&card5=900000001&name=900006840&dw=%B3%F8%C1%F4%CF%E3%B4%A8%B2%CB%BB%F0%B9%F8&card4=900000002&card2=900006840&card%5Fno1=900006840&shopid1=900000003&staff%5Fgrade=1&reg%5Fnbr=900006840&card3=900000003'"
+                //     },
+                //     function(values) {
+                //         $window.alert(values.toString());
+                //     }
+                // );
+                // inAppBrowser.executeScript({code: 'document.cookie="hello_world";alert(document.cookie)'},
+                //     function(){
+                //         $window.alert('executeScript');
+                //     }
+                // );
+                //console.log("did it");    //====================test
+                if($scope.loadOpen instanceof Function){
+                    //inAppBrowser.addEventListener('loadstart', wrappedFunction($scope.loadOpen));
+                    //inAppBrowser.addEventListener('loadstart', function(event){ alert(event.type+': '+event.url); });
+                    inAppBrowser.addEventListener('loadstart', inAppBrowserStart);
+                    //console.log($scope.loadStart);    //====================test
+                }
+                if($scope.loadStop instanceof Function){
+                    inAppBrowser.addEventListener('loadstop', inAppBrowserStop);
+                    //console.log($scope.loadStop);    //====================test
+                }
+                if($scope.loadError instanceof Function){
+                    inAppBrowser.addEventListener('loaderror', $scope.loadError);
+                    //console.log($scope.loadError);    //====================test
+                }
+                if($scope.exit instanceof Function){
+                    inAppBrowser.addEventListener('exit', inAppBrowserClosed);
+                    //console.log($scope.exit);    //====================test
+                    //$scope.exit(); //====================test
+                    //var action = wrappedFunction($scope.exit);  //====================test
+                    //console.log(action);    //====================test
+                    // var wrapped = function(action){
+                    //                 return action();
+                    //               };
+                    // wrapped($scope.exit);
+                    // console.log(wrapped($scope.exit));    //====================test
+                    //action();   //====================test
+                    //console.log($scope.$id);    //====================test
+                }
+                //console.log(inAppBrowser);    //====================test
+            };
+
+            // $scope.$on("$destroy", function(){
+            //     console.log(inAppBrowser);    //====================test
+            //     if(inAppBrowser != null){
+            //         //console.log("did it");    //====================test
+            //         inAppBrowser.removeEventListener('exit', wrappedFunction($scope.exit));
+
+            //         if($scope.exit){
+            //             inAppBrowser.removeEventListener('exit', wrappedFunction($scope.exit));
+            //         }
+            //         if($scope.loadStart){
+            //             inAppBrowser.removeEventListener('loadstart', wrappedFunction($scope.loadStart));
+            //         }
+            //         if($scope.loadStop){
+            //             inAppBrowser.removeEventListener('loadstop', wrappedFunction($scope.loadStop));
+            //         }
+            //         if($scope.loadError){
+            //             inAppBrowser.removeEventListener('loaderror', wrappedFunction($scope.loadError));
+            //         }
+            //     }
+
+            // });
+        }
+    };
+});
