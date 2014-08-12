11953acd3524e4977b85aab92e264cff1f8c7574
alexgzhou <alexgzhou@163.com>
2014-08-13 00:58:55 +0800

    单击按钮可以成功写入，需要自动写入
    原因：iframe中的源是远程的，加载需要时间，还没加载完成本地就执行postMessage方法，导致出现如下错误。
    Failed to execute 'postMessage' on 'DOMWindow':
    The target origin provided ('http://17f.go5le.net') does not match
    the recipient window's origin ('https://10.12.43.168').

---
+++ PHPServ/postMessage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script>
+        window.onload=function(){
+            function handMessage(event){
+                event = event || window.event;
+                //验证是否来自预期内的域，如果不是不做处理，这样也是为了安全方面考虑
+                //if(event.origin === 'http://www.postmessage1.com'){
+                    document.getElementById('divMessage').innerHTML = event.data;
+		    alert(event.data);
+                //}
+            }
+            //给window对象绑定message事件处理
+            if(window.addEventListener){
+                window.addEventListener("message", handMessage, false);
+            }
+            else{
+                window.attachEvent("onmessage", handMessage);
+            }      
+        }        
+    </script>
+</head>
+<body>
+我是不同域的iframe页面，下面是接受到的消息内容
+<div id="divMessage"></div>
+</body>
+</html>
