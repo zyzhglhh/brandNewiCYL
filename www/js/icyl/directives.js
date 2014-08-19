243d132a7af928d99e243bbf3ac4cdca10462f2a
alexgzhou <alexgzhou@163.com>
2014-08-19 17:25:55 +0800

    bugfix#tiny

--- www/js/icyl/directives.js
+++ www/js/icyl/directives.js
@@ -125,10 +125,10 @@ angular.module('icyl.directives', [])
                     var sharing = Storage.kget('socialSharing').split("<$separate$>");
                     //window.alert(sharing);    //===============test
                     window.plugins.socialsharing.share(
-                        null,   //!!sharing[0] ? sharing[0] : '这个平台不错的！',   //'信息、主题图片和链接', 
-                        null,   //!!sharing[1] ? sharing[1] : '请关注这个平台！',   //'主题', 
-                        null,   //!!sharing[2] ? sharing[2] : null,     //'图片地址',
-                        !!sharing[3] ? sharing[3] : 'http://17f.go5le.net/bootstrap-3.1.1/',    //'网址',
+                        !!sharing[2] ? sharing[2] : '这个平台不错的！',   //'信息、主题图片和链接', 
+                        '智慧团青:',   //!!sharing[1] ? sharing[1] : '请关注这个平台！',   //'主题', 
+                        !!sharing[3] ? sharing[3] : null,     //'图片地址',
+                        !!sharing[0] ? sharing[0] : 'http://17f.go5le.net/bootstrap-3.1.1/',    //'网址',
                         function (result) {
                             console.log('result: ' + result);
                             //window.alert('result: ' + result);    //===============test
